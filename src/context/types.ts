5a0e9fbb760c744f9a6ae0b61d18ee2f11ada0d4
Uwase Djaria <uwasedjaria41@gmail.com>
2025-11-01 19:29:04 +0200

    update types with interface

---
+++ src/context/types.ts
@@ -0,0 +1,27 @@
+export interface Client{
+
+id: string
+name: string
+country: string
+email?: string
+
+}
+
+export interface Project{
+
+id: string
+clientId: string
+title: string
+budget: number
+status: "pending" | "in-progress" | "completed"
+paymentStatus: "paid" | "unpaid"
+
+}
+
+export interface Payment{
+
+projectId: string
+amount: number
+date: string
+
+}
