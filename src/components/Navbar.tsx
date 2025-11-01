65c8a1d58f584b38207de5288ff69c33c6c39c5e
Uwase Djaria <uwasedjaria41@gmail.com>
2025-11-01 18:54:05 +0200

    update navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,14 @@
-import React from "react";
+
 import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <nav className="bg-red-900 text-white p-4 rounded mb-6 flex justify-between items-center">
+    <nav className="bg-blue-700 text-white p-4 rounded mb-6 flex justify-between items-center">
       <h1 className="text-xl font-bold">Mini Dashboard</h1>
       <div className="space-x-4">
-        <Link to="/" className="hover:underline">
-          Dashboard
-        </Link>
-        <Link to="/clients" className="hover:underline">
-          Clients
-        </Link>
-        <Link to="/projects" className="hover:underline">
-          Projects
-        </Link>
+        <Link to="/" className="hover:underline">Dashboard</Link>
+        <Link to="/clients" className="hover:underline">Clients</Link>
+        <Link to="/projects" className="hover:underline">Projects</Link>
       </div>
     </nav>
   );
